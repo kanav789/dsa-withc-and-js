82b0e642b1cff21f35d323b13523876028dc5538
kanav789 <kkanav73@gmamil.com>
2024-07-17 09:47:25 +0530

    RemoveElement and removeDuplicates

--- Array/CheckThearrayispresent.js
+++ Array/CheckThearrayispresent.js
@@ -1,11 +1,11 @@
-function checkArrayisSorted(arr) {
+function checksortedarray(arr) {
   for (let i = 1; i < arr.length; i++) {
     if (arr[i - 1] > arr[i]) {
       return false;
     }
   }
+
   return true;
 }
-
-let arr = [1, 2, 1, 3, 4];
-console.log(checkArrayisSorted(arr));
+const arr = [1, 2, 3, 1];
+console.log(checksortedarray(arr));
--- Array/SecondLargest.js
+++ Array/SecondLargest.js
@@ -1,17 +1,42 @@
-const arr = [3, 2, 1, 5, 2];
-let largest = arr[0];
-for (let i = 0; i <= arr.length - 1; i++) {
-  if (largest < arr[i]) {
-    largest = arr[i];
+// const arr = [3, 2, 1, 5, 2];
+// let largest = arr[0];
+// for (let i = 0; i <= arr.length - 1; i++) {
+//   if (largest < arr[i]) {
+//     largest = arr[i];
+//   }
+// }
+
+// console.log("Largest Element is", largest);
+
+// let Secondlargest = -1;
+// for (let b = 0; b <= arr.length - 1; b++) {
+//   if (arr[b] > Secondlargest && arr[b] != largest) {
+//     Secondlargest = arr[b];
+//   }
+// }
+// console.log("second largest element", Secondlargest);
+
+function Largest(arr) {
+  let largest = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (largest < arr[i]) {
+      largest = arr[i];
+    }
   }
+  return largest;
 }
 
-console.log("Largest Element is", largest);
-
-let Secondlargest = -1;
-for (let b = 0; b <= arr.length - 1; b++) {
-  if (arr[b] > Secondlargest && arr[b] != largest) {
-    Secondlargest = arr[b];
+function secondLargest(arr, largest) {
+  let SLargest = -1;
+  for (let b = 1; b < arr.length; b++) {
+    if (arr[b] > SLargest && arr[b] != largest) {
+      SLargest = arr[b];
+    }
   }
+  return SLargest;
 }
-console.log("second largest element", Secondlargest);
+
+let arr = [1, 2, 3, 4, 3, 7];
+let largest = Largest(arr);
+console.log("largest", largest);
+console.log(secondLargest(arr, largest));
