01e8d4da4b76a97e109247165c036b900fc4bb2c
kanav789 <kkanav73@gmamil.com>
2024-08-29 10:21:32 +0530

    peak element

---
+++ BinarySearch/Peakelement.js
@@ -0,0 +1,18 @@
+function peakElement(arr) {
+  let s = 0;
+  let e = arr.length - 1;
+
+  while (s < e) {
+    let mid = Math.floor((s + e) / 2);
+    if (arr[mid] > arr[mid + 1]) {
+      e = mid;
+    } else {
+      s = mid + 1;
+    }
+  }
+  return s;
+}
+
+let arr = [1, 2, 1, 3, 5, 6, 4];
+
+console.log(peakElement(arr), "kya");
